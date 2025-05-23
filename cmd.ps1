7b0e224e18419e59d27731f499f02829bfa9b93c
Ziya Mirzayev <82753981+ziyamrzyv@users.noreply.github.com>
2025-05-23 19:35:48 +0400

    Update cmd.ps1

--- cmd.ps1
+++ cmd.ps1
@@ -1,4 +1,4 @@
 Write-Host "Hacklandin"
 Write-Host "HA HA HA"
 $os = Get-CimInstance -ClassName Win32_OperatingSystem
-Write-Host "$($os.Caption) - your OS system."
+Write-Host "Your OS is $($os.Caption)."
