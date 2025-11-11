df6148a6d6df56c58e5acaa3e5626bd7b2190a12
ziyamrzyv <ziyamirzeyev19@gmail.com>
2025-11-11 13:48:25 +0400

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: 'class', // isterse kaldırılabilir
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
