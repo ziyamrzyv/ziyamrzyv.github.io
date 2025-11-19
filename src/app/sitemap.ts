82a691fe9f6f69d8882ac8d909feb447dc085f7d
ziyamrzyv <ziyamirzeyev19@gmail.com>
2025-11-20 00:55:29 +0400

    Update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://ziyarmrzyv.me/",
+      url: "https://ziyarmrzyv.me/sitemap.xml",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1.0,
